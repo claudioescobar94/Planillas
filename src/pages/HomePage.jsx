03b3d0ed5f3e948a3948ac283dc84011136bd5fa
Claudio <escobarc697@gmail.com>
2025-08-24 21:00:24 -0300

    ..

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import KpiLotoChart from '../components/KpiLotoChart';
-import KpiParadasChart from '../components/KpiParadasChart';
-import KpiMaquinasChart from '../components/KpiMaquinasChart';
 
 export default function HomePage() {
   return (
